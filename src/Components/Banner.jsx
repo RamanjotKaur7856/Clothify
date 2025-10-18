9df7c6a94e021923bd29f1e25020dc49466d075c
Ramanjot Kaur <156178826+RamanjotKaur7856@users.noreply.github.com>
2025-10-18 13:29:08 +0530

    banner

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Banner() {
   return (
     <>
-      <img src="images/mainbanner.jpg" alt="Main Banner" style={{ width: '100%' }} />
+      <img src="assets/mainbanner.jpg" alt="Main Banner" style={{ width: '100%' }} />
       <h2 style={{ fontSize: '40px', textAlign: 'center', paddingTop: '20px' }}>Shop The Look</h2>
     </>
   );
